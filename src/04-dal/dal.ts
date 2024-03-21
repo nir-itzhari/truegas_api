922abde289dc7e0b9bc9aa9777d5e50ffa3ba40d
nir-itzhari <synexprox@gmail.com>
2024-03-21 17:37:11 +0200

    convert to new promise

--- src/04-dal/dal.ts
+++ src/04-dal/dal.ts
@@ -6,46 +6,67 @@ import os from 'os';
 let isConnected = false;
 
 
-const connect = async (): Promise<void> => {
-    if (isConnected) {
-        console.log('Already connected to MongoDB');
-        return;
-    }
-    try {
-        console.log(os.hostname())
-        const db = await mongoose.connect(os.hostname() === 'truegasdocker' ? config.dockerConnectionString : config.connectionString)
-        console.log("We're connected to MongoDB " + db.connections[0].name)
-        isConnected = true;
-    } catch (error: any) {
-        console.error('Error connecting to MongoDB:', error.message);
-        throw new ErrorModel(400, error.message);
-    }
+const connect = (): Promise<void> => {
+    return new Promise<void>((resolve, reject) => {
+        if (isConnected) {
+            console.log('Already connected to MongoDB');
+            resolve();
+            return;
+        }
+        try {
+            console.log(os.hostname());
+            mongoose.connect(os.hostname() === 'truegasdocker' ? config.dockerConnectionString : config.connectionString)
+                .then((db) => {
+                    console.log("We're connected to MongoDB " + db.connections[0].name);
+                    isConnected = true;
+                    resolve();
+                })
+                .catch((error) => {
+                    console.error('Error connecting to MongoDB:', error.message);
+                    reject(new ErrorModel(400, error.message));
+                });
+        } catch (error) {
+            console.error('Error connecting to MongoDB:', error.message);
+            reject(new ErrorModel(400, error.message));
+        }
+    });
+};
+
+
+
+export function closeDatabaseConnection(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        if (!isConnected) {
+            console.log('Not connected to MongoDB');
+            resolve();
+            return;
+        }
+
+        mongoose.disconnect()
+            .then(() => {
+                isConnected = false;
+                console.log('Disconnected from MongoDB');
+                resolve();
+            })
+            .catch((error) => {
+                console.error('Error disconnecting from MongoDB:', error.message);
+                reject(new ErrorModel(400, error.message));
+            });
+    });
 }
 
-
-
-export async function closeDatabaseConnection(): Promise<void> {
-    if (!isConnected) {
-        console.log('Not connected to MongoDB');
-        return;
-    }
-
-    try {
-        await mongoose.disconnect();
-        isConnected = false;
-        console.log('Disconnected from MongoDB');
-    } catch (error: any) {
-        console.error('Error disconnecting from MongoDB:', error.message);
-        throw new ErrorModel(400, error.message);
-    }
-}
-
-
-process.on('SIGINT', async (): Promise<void> => {
+process.on('SIGINT', () => {
     console.log('Received SIGINT signal, closing MongoDB connection');
-    await closeDatabaseConnection();
-    process.exit(1);
+    closeDatabaseConnection()
+        .then(() => {
+            process.exit(0);
+        })
+        .catch((error) => {
+            console.error('Error handling SIGINT:', error.message);
+            process.exit(1);
+        });
 });
+
 export default {
     connect,
     // mailSend
